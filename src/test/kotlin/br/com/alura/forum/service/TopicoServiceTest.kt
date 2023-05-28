9be3cb520c69ee5ae50fb7fc6f7f5e4568e85058
HugoBertolassi <hugordura@hotmail.com>
2023-05-28 12:05:33 -0400

    feat/ teste do metodo finAlld,verificacao do valor do topico válido durante a transformação de Topico view, teste se valor da exceção esta correto

--- src/test/kotlin/br/com/alura/forum/service/TopicoServiceTest.kt
+++ src/test/kotlin/br/com/alura/forum/service/TopicoServiceTest.kt
@@ -1,5 +1,6 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.exception.NotFoundException
 import br.com.alura.forum.mapper.TopicoFormMapper
 import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.Topico
@@ -8,11 +9,16 @@ import br.com.alura.forum.model.TopicoViewTest
 import br.com.alura.forum.repository.TopicoRepository
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
+import java.util.*
+import kotlin.collections.ArrayList
 
 class TopicoServiceTest {
     //variaveis de teste
@@ -25,8 +31,14 @@ class TopicoServiceTest {
     val topicos:List<Topico> = ArrayList()
     val topicoRepository: TopicoRepository = mockk{
         every { findByCursoNome(any(),any())} returns topico
+        every { findAll(paginacao)} returns topico
     }
-    val topicoViewMapper:TopicoViewMapper = mockk()
+    val slot = slot<Topico>()//config de captura antes da modificação
+    val topicoViewMapper:TopicoViewMapper = mockk(){
+        every{map(any())} returns TopicoViewTest.build()
+        every { map(capture(slot)) } returns TopicoViewTest.build()
+
+    }
     val topicoFormMapper:TopicoFormMapper = mockk()
 
     //oque realmente quero testar
@@ -36,12 +48,47 @@ class TopicoServiceTest {
 
     @Test
     fun `deve listar topicos a partir do nome do curso`(){
-        every{topicoViewMapper.map(any())} returns TopicoViewTest.build()
+        //val slot = slot<Topico>()//função slot permite capturar a chamada e verificar os valores recebidos
+        //every { topicoViewMapper.map(capture(slot)) } returns TopicoViewTest.build()
+
+        val topico = TopicoTest.build() //cria opico para verificar com o valor capturado durante a trasnformação para mapper
         topicoService.listar("Kotlin", paginacao)
 
         verify(exactly = 1){topicoRepository.findByCursoNome(any(),any())}
         verify(exactly = 1){topicoViewMapper.map(any())}
         verify(exactly = 0){topicoRepository.findAll(paginacao)}
 
+        //assim é possivel verificar se o tipo Topico está correto
+        assertThat(slot.captured.titulo).isEqualTo(topico.titulo)
+        assertThat(slot.captured.mensagem).isEqualTo(topico.mensagem)
+        assertThat(slot.captured.status).isEqualTo(topico.status)
+    }
+
+    @Test
+    fun `deve listar topicos quando o nome do curso for nulo`(){
+
+        val topico = TopicoTest.build()
+
+        topicoService.listar(null, paginacao)
+
+        verify(exactly = 0){topicoRepository.findByCursoNome(any(),any())}
+        verify(exactly = 1){topicoViewMapper.map(any())}
+        verify(exactly = 1){topicoRepository.findAll(paginacao)}
+
+        //assim é possivel verificar se o tipo Topico está correto
+        assertThat(slot.captured.titulo).isEqualTo(topico.titulo)
+        assertThat(slot.captured.mensagem).isEqualTo(topico.mensagem)
+        assertThat(slot.captured.status).isEqualTo(topico.status)
+    }
+
+    @Test
+    fun `deve lancar excecao se nao achar topico por id`() {
+        every { topicoRepository.findById(any()) } returns Optional.empty()
+
+        val actual = assertThrows<NotFoundException> {
+            topicoService.buscarPorId(2)
+        }
+
+        assertThat(actual.message).isEqualTo("Topico não encontrado")
     }
 }
